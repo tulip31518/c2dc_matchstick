0b38b6c49c673c4bad797646cb05d20a9e96f137
unknown <tulip31518@outlook.com>
2018-09-07 21:10:39 +0800

    first

---
+++ assets/scripts/game.js
@@ -0,0 +1,71 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+
+        btn_start:{
+            default:null,
+            type: cc.Node
+        },
+
+        daily_gift:{
+            default:null,
+            type: cc.Node
+        },
+
+        btn_sound:{
+            default:null,
+            type: cc.Node
+        },
+
+        btn_cap:{
+            default:null,
+            type: cc.Node
+        },
+
+        btn_bucket:{
+            default:null,
+            type: cc.Node
+        },
+
+        btn_share:{
+            default:null,
+            type: cc.Node
+        },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
